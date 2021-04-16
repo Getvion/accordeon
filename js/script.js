8dad2fb8febdec66a036864c7c9d456955588826
Getvion <gog0125@mail.com>
2021-04-16 11:40:53 +0300

    wrote script.js, remove div-title in .html, rewrite styles in style.css

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-console.log('Hello World!');
+const headers = document.querySelectorAll('.block__item-title');
+
+headers.forEach(function (item) {
+
+    item.addEventListener('click', function () {
+
+        this.nextElementSibling.classList.toggle('block__hidden');
+    })
+});
